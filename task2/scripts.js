61dc9912aa67307db43db27d712325c165123687
YBryn <139452185+YBryn@users.noreply.github.com>
2023-08-19 19:15:31 +0300

    Update scripts.js

--- task2/scripts.js
+++ task2/scripts.js
@@ -1,27 +1,45 @@
 "use strict";
 let toggleButton = document.querySelector('#toggleButton');
+const storedTheme = localStorage.getItem('theme');
+const lastAction = localStorage.getItem('lastAction');
+
+if (storedTheme) {
+    document.body.classList.add(storedTheme);
+}
+if (lastAction) {
+    document.querySelector(storedTheme === 'dark' ? '#lastTrnOffDate' : '#lastTrnOnDate').textContent = lastAction;
+}
 
 toggleButton.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     const dateNow = new Date();
+    const formatdDate = formatDate(dateNow);
+    const formatdTime = formatTime(dateNow);
+    const actionText = `Last time ${document.body.classList.contains('dark') ? 'OFF' : 'ON'}: ${formatdDate} at ${formatdTime} sec`;
+    
+    localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : '');
+    localStorage.setItem('lastAction', actionText);
+    
+    document.querySelector('.container h2').textContent = actionText;
+    toggleButton.textContent = document.body.classList.contains('dark') ? 'Turn ON' : 'Turn OFF';
+});
 
-    const day = dateNow.getDate();
-    const month = dateNow.getMonth() + 1;
+window.addEventListener('beforeunload', () => {
+    localStorage.removeItem('theme');
+    localStorage.removeItem('lastAction');
+});
+
+function formatDate(date) {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
     const formattedDay = day < 10 ? `0${day}` : day;
     const formattedMonth = month < 10 ? `0${month}` : month;
-    
-    const dates = `${formattedDay}-${formattedMonth}-${dateNow.getFullYear()}`;
+    return `${formattedDay}-${formattedMonth}-${date.getFullYear()}`;
+}
 
-    const hours = dateNow.getHours() < 10 ? `0${dateNow.getHours()}` : dateNow.getHours();   
-    const minutes = dateNow.getMinutes() < 10 ? `0${dateNow.getMinutes()}` : dateNow.getMinutes();
-    const seconds = dateNow.getSeconds() < 10 ? `0${dateNow.getSeconds()}` : dateNow.getSeconds();
-    
-    const date = `${hours}:${minutes}:${seconds}`;
-    if (document.body.classList.contains('dark')) {
-        toggleButton.textContent = 'Turn ON';
-        document.querySelector('#lastTrnOffDate').textContent = `Last time OFF: ${dates} at ${date} sec`;
-    } else {
-        toggleButton.textContent = 'Turn OFF';
-        document.querySelector('#lastTrnOnDate').textContent = `Last time ON: ${dates} at ${date} sec`;
-    }
-});
+function formatTime(date) {
+    const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();   
+    const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+    const seconds = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds();
+    return `${hours}:${minutes}:${seconds}`;
+}
