fa223d4d9d7cda3b48a46c800392bfd8011909f5
YBryn <139452185+YBryn@users.noreply.github.com>
2023-08-20 23:58:30 +0300

    Update scripts.js

--- task2/scripts.js
+++ task2/scripts.js
@@ -1,31 +1,34 @@
 "use strict";
+
 let toggleButton = document.querySelector('#toggleButton');
 const storedTheme = localStorage.getItem('theme');
 const lastAction = localStorage.getItem('lastAction');
+const button = localStorage.getItem('button');
 
 if (storedTheme) {
     document.body.classList.add(storedTheme);
-}
-if (lastAction) {
+};
+if (lastAction) {   
     document.querySelector(storedTheme === 'dark' ? '#lastTrnOffDate' : '#lastTrnOffDate').textContent = lastAction;
-}
+};
+if (button) {
+    toggleButton.textContent = button;
+};
 
 toggleButton.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     const dateNow = new Date();
     const formatdDate = formatDate(dateNow);
     const formatdTime = formatTime(dateNow);
-    const actionText = `Last time ${document.body.classList.contains('dark') ? 'OFF' : 'ON'}: ${formatdDate} at ${formatdTime} sec`;
-    
-    localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : '');
-    localStorage.setItem('lastAction', actionText);
+    const actionText = `Last time ${document.body.classList.contains('dark') ? 'OFF' : 'ON'}: ${formatdDate} at ${formatdTime} sec`; 
+    const newButton = document.body.classList.contains('dark') ? 'Turn ON' : 'Turn OFF'; 
+
+    localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : '');// тема
+    localStorage.setItem('lastAction', actionText); // дата
+    localStorage.setItem('button', newButton); // кнопка
     
     document.querySelector('.container h2').textContent = actionText;
-    toggleButton.textContent = document.body.classList.contains('dark') ? 'Turn ON' : 'Turn OFF';
-});
-
-window.addEventListener('beforeunload', () => {
-    localStorage.removeItem('theme');
+    toggleButton.textContent = newButton;
 });
 
 function formatDate(date) {
@@ -34,11 +37,10 @@ function formatDate(date) {
     const formattedDay = day < 10 ? `0${day}` : day;
     const formattedMonth = month < 10 ? `0${month}` : month;
     return `${formattedDay}-${formattedMonth}-${date.getFullYear()}`;
-}
-
+};
 function formatTime(date) {
     const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();   
     const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
     const seconds = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds();
     return `${hours}:${minutes}:${seconds}`;
-}
+};
